93943ea62e65311c5487ca981f730a23a96eb83b
Oleksii Yeyo <alexaveliukraine@gmail.com>
2023-12-11 00:51:41 +0200

    Implemented server using Express with ES Modules and integrated nodemon

--- data/mock-data-face-news.js
+++ data/mock-data-face-news.js
@@ -9,6 +9,11 @@ const fakeNews = [
       title: 'New Study Finds Chocolate is Healthy!',
       content: 'Scientists reveal surprising health benefits of eating chocolate.',
     },
+    {
+      id: 3,
+      title: 'Nuclear power plant has exploded in Ukraine',
+      content: 'It was in 1986 at Chernobyl city.',
+    },
     // Add more fake news items as needed
   ];
 
